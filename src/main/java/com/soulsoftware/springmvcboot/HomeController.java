4e1d8c835cc159e0d45e9bc48d7348aadb2c3021
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2021-01-13 12:05:47 +0100

    prefix and suffix to mvc.view

--- src/main/java/com/soulsoftware/springmvcboot/HomeController.java
+++ src/main/java/com/soulsoftware/springmvcboot/HomeController.java
@@ -21,7 +21,7 @@ public class HomeController {
     public ModelAndView add(@RequestParam("num1") int i,@RequestParam("num2") int j){
 
         ModelAndView mv=new ModelAndView();
-        mv.setViewName("result.jsp");
+        mv.setViewName("result");
         int num3=i+j;
         mv.addObject("num3",num3);
 
