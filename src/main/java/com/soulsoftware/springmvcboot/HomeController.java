65408f9a279ca3dcb8c5167063ca84a6e3af27b0
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2021-01-13 11:58:35 +0100

    ModelAndView

--- src/main/java/com/soulsoftware/springmvcboot/HomeController.java
+++ src/main/java/com/soulsoftware/springmvcboot/HomeController.java
@@ -3,6 +3,7 @@ package com.soulsoftware.springmvcboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -17,11 +18,13 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int i,@RequestParam("num2") int j, HttpSession session){
+    public ModelAndView add(@RequestParam("num1") int i,@RequestParam("num2") int j){
 
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("result.jsp");
         int num3=i+j;
+        mv.addObject("num3",num3);
 
-        session.setAttribute("num3",num3);
-        return "result.jsp";
+        return mv;
     }
 }
