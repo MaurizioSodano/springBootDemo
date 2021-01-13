fa49edef6d2439ea4001a1a874f917f4208046cf
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2021-01-13 15:29:53 +0100

    Added PostRequest and GetRequest

--- src/main/java/com/soulsoftware/springmvcboot/HomeController.java
+++ src/main/java/com/soulsoftware/springmvcboot/HomeController.java
@@ -2,41 +2,49 @@ package com.soulsoftware.springmvcboot;
 
 import com.soulsoftware.springmvcboot.model.Alien;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
     @ModelAttribute
-    public void modelData(Model m){
-        m.addAttribute("name","Aliens");
+    public void modelData(Model m) {
+        m.addAttribute("name", "Aliens");
     }
+
     @RequestMapping("/")
-    public String home(){
+    public String home() {
 
         return "index";
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, Model m){
+    public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, Model m) {
 
 
-        int num3=i+j;
-        m.addAttribute("num3",num3);
+        int num3 = i + j;
+        m.addAttribute("num3", num3);
+
+        return "result";
+    }
+
+    @PostMapping("addAlien")
+    public String addAlien(@ModelAttribute("alien") Alien a) {
+
 
         return "result";
     }
 
-    @RequestMapping("addAlien")
-    public String addAlien(@ModelAttribute("alien") Alien a){
-
-
-        return "result";
+    @GetMapping("getAliens")
+    public String getAliens(Model m) {
+        List<Alien> aliens= Arrays.asList(new Alien(101,"Pippo"),new Alien(200,"Pluto"));
+        m.addAttribute("result",aliens.toString());
+        return "showAliens";
     }
+
 }
--- src/main/webapp/views/index.jsp
+++ src/main/webapp/views/index.jsp
@@ -9,7 +9,7 @@
 <body>
 	Welcome to SoulSoftware
 
-	<form action="addAlien">
+	<form action="addAlien" method="post">
 	    Enter your id : <input type="text" name="aid"><br>
 	    Enter your name : <input type="text" name="aname"><br>
 	    <input type="submit">
