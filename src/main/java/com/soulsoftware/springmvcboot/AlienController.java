25a1cc232c80aaa7df922d174e338b515d97a419
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2021-01-18 16:35:17 +0100

    REST @GetMapping

---
+++ src/main/java/com/soulsoftware/springmvcboot/AlienController.java
@@ -0,0 +1,25 @@
+package com.soulsoftware.springmvcboot;
+
+import com.soulsoftware.springmvcboot.model.Alien;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class AlienController {
+    @Autowired
+    AlienRepo repo;
+
+    @GetMapping("aliens")
+    @ResponseBody
+    public  List<Alien> getAliens(Model m) {
+        // List<Alien> aliens= Arrays.asList(new Alien(101,"Pippo"),new Alien(200,"Pluto"));
+        List<Alien> aliens = repo.findAll();
+
+        return aliens;
+    }
+}
