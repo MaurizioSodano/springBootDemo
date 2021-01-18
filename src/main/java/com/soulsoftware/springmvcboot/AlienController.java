530722ae82fe8752e1a4767e51b84c740754427c
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2021-01-18 17:22:47 +0100

    REST get element PathVariable

--- src/main/java/com/soulsoftware/springmvcboot/AlienController.java
+++ src/main/java/com/soulsoftware/springmvcboot/AlienController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -21,5 +22,12 @@ public class AlienController {
         List<Alien> aliens = repo.findAll();
 
         return aliens;
+    }
+
+    @GetMapping("alien/{aid}")
+    @ResponseBody
+    public Alien getAlien(@PathVariable("aid")  int aid){
+        Alien alien= repo.findById(aid).orElse(new Alien(0,""));
+        return alien;
     }
 }
