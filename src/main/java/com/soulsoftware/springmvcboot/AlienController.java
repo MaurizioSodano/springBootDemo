dde6d54a9922aebc57fa07e02446ae3d61868a26
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2021-01-18 17:35:41 +0100

    using @RestController

--- src/main/java/com/soulsoftware/springmvcboot/AlienController.java
+++ src/main/java/com/soulsoftware/springmvcboot/AlienController.java
@@ -2,22 +2,22 @@ package com.soulsoftware.springmvcboot;
 
 import com.soulsoftware.springmvcboot.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class AlienController {
     @Autowired
     AlienRepo repo;
 
     @GetMapping("aliens")
-    @ResponseBody
-    public  List<Alien> getAliens(Model m) {
+    public List<Alien> getAliens(Model m) {
         // List<Alien> aliens= Arrays.asList(new Alien(101,"Pippo"),new Alien(200,"Pluto"));
         List<Alien> aliens = repo.findAll();
 
@@ -25,9 +25,8 @@ public class AlienController {
     }
 
     @GetMapping("alien/{aid}")
-    @ResponseBody
-    public Alien getAlien(@PathVariable("aid")  int aid){
-        Alien alien= repo.findById(aid).orElse(new Alien(0,""));
+    public Alien getAlien(@PathVariable("aid") int aid) {
+        Alien alien = repo.findById(aid).orElse(new Alien(0, ""));
         return alien;
     }
 }
